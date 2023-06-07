4028113a73de83f92ebe31e82db345b4fd61e827
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-06-07 18:45:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Products } from './pages/Products';
+import { Contact } from './pages/Contact';
+import { NavBar } from './components/NavBar';
+import { useState, createContext } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const AppContext = createContext()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <QueryClientProvider>
+        <Router>
+            <NavBar/>
+          <Routes>
+            <Route path="/" element={<Home/>}>
+            <Route path="/Products" element={<Products/>}>
+            <Route path="/Contact" element={<Contact/>} >
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import { Axios } from "axios";
+
+export const Home = () => {
+    
+}
